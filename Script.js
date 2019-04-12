de5de4142a9015a825108575d16242fbf979b46f
Gohan19266 <johanmamani@upeu.edu.pe>
2019-04-11 21:00:49 -0500

    asd
    asd

---
+++ Script.js
@@ -0,0 +1,37 @@
+function inicio(){
+    document.getElementById("idinicio").style.display="block";
+    document.getElementById("idventas").style.display="none";
+    document.getElementById("idreporte").style.display="none";
+    document.getElementById("idcliente").style.display="none";
+    document.getElementById("idproducto").style.display="none";
+
+}
+function ventas(){
+    document.getElementById("idinicio").style.display="none";
+    document.getElementById("idventas").style.display="block";
+    document.getElementById("idreporte").style.display="none";
+    document.getElementById("idcliente").style.display="none";
+    document.getElementById("idproducto").style.display="none";
+}
+function reporte(){
+    document.getElementById("idinicio").style.display="none";
+    document.getElementById("idventas").style.display="block";
+    document.getElementById("idreporte").style.display="none";
+    document.getElementById("idcliente").style.display="none";
+    document.getElementById("idproducto").style.display="none";
+}
+function cliente(){
+    document.getElementById("idinicio").style.display="none";
+    document.getElementById("idventas").style.display="none";
+    document.getElementById("idreporte").style.display="none";
+    document.getElementById("idcliente").style.display="block";
+    document.getElementById("idproducto").style.display="none";
+
+}
+function producto(){
+    document.getElementById("idinicio").style.display="none";
+    document.getElementById("idventas").style.display="block";
+    document.getElementById("idreporte").style.display="none";
+    document.getElementById("idcliente").style.display="none";
+    document.getElementById("idproducto").style.display="none";
+}
